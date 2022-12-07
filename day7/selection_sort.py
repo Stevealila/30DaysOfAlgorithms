a12ff043c2236503cbbef6847842aec826723892
Stevealila <stevealila25@gmail.com>
2022-12-07 07:11:13 +0300

    Selection Sort with py

---
+++ day7/selection_sort.py
@@ -0,0 +1,21 @@
+"""
+Locate the minimum element and append it to second list,
+while removing the element from its initial parent.
+Repeat the process until the original list is empty.
+
+Runs in 0(n*2)
+"""
+
+def selectionSort(arr):
+    sorted_arr = []
+
+    while len(arr) > 0:
+        smallest = min(arr)
+        sorted_arr.append(smallest)
+        arr.remove(smallest)
+
+    return sorted_arr
+
+
+arr = [19, 13, 17, 15, 11]
+print(selectionSort(arr))
