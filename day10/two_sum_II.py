74e4d0050501da57cdd802052e78f19e3f4abd7c
Stevealila <stevealila25@gmail.com>
2022-12-10 06:40:05 +0300

    Two Sum II with py

---
+++ day10/two_sum_II.py
@@ -0,0 +1,39 @@
+"""
+Question: Leetcode 167
+Link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+
+This implementation relies on the Two Pointers Technique.
+"""
+
+def twoSumII(arr, target):
+    # Initially reference the first and last indices with left and right pointers. 
+    left, right = 0, len(arr)-1
+    """
+    Keep reassigning the pointers,
+    while summing them and 
+    comparing the sum with target
+    until (1) target found or (2) the pointers collide without find the target.
+    """
+    while left <= right:
+        # sum the two pointers
+        sum = arr[left] + arr[right] 
+        # target found
+        if sum == target: return [left+1, right+1]
+        # otherwise, reassign the pointers
+        if sum < target: 
+            left += 1
+        else: 
+            right -= 1
+
+    # no matching indices
+    return []
+
+
+numbers = [2, 7, 11, 15]
+print(twoSumII(numbers, 9)) #[1, 2]
+
+numbers = [2, 3, 4]
+print(twoSumII(numbers, 6)) #[1, 3]
+
+numbers = [-1, 0]
+print(twoSumII(numbers, -1)) #[1, 2]
